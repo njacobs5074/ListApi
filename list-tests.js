733aff993d19b0d611808dd6ce60a3c19ed4f663
njacobs5074 <nicholas_jacobs@yahoo.com>
2015-04-10 21:14:34 +0200

    All tests working, added tests for multi-character strings, and faked numbers for single digit inputs

--- list-tests.js
+++ list-tests.js
@@ -1,92 +1,128 @@
-
 QUnit.test("A new list is empty", function (assert) {
-	var l = new List()
-  assert.equal(l.empty(), true)
-  assert.equal(l.head(), null)
+    var l = new List();
+    assert.equal(l.empty(), true);
+    assert.equal(l.head(), null);
 });
 
 QUnit.test("It is really a list", function (assert) {
-  var l = new List()
-  l.add("a")
-  l.add("b")
-  l.add("c")
-  assert.equal(l.item.data, "c")
-  assert.equal(l.item.tail.data, "b")
-  assert.equal(l.item.tail.tail.data, "a")
-  assert.equal(l.item.tail.tail.tail, null)
+    var l = new List();
+    l.add("a");
+    l.add("b");
+    l.add("c");
+    assert.equal(l.item.data, "c");
+    assert.equal(l.item.tail.data, "b");
+    assert.equal(l.item.tail.tail.data, "a");
+    assert.equal(l.item.tail.tail.tail, null);
 });
 
 QUnit.test("Head of the list contains most recent addition", function (assert) {
-  var l = new List()
-  l.add("a")
-  assert.equal(l.head(), "a")
-  l.add("b")
-  assert.equal(l.head(), "b")
-  l.add("c")
-  l.add("d")
-  assert.equal(l.head(),"d")
+    var l = new List();
+    l.add("a");
+    assert.equal(l.head(), "a");
+    l.add("b");
+    assert.equal(l.head(), "b");
+    l.add("c");
+    l.add("d");
+    assert.equal(l.head(), "d");
 });
 
 QUnit.test("Returns the correct length of a list", function (assert) {
-  var l = new List()
-  assert.equal(l.length(), 0)
-  l.add("a")
-  l.add("b")
-  l.add("c")
-  assert,equal(l.length(),3)
-  l.pop()
-  assert,equal(l.length(),2)
-  l.pop()
-  l.pop()
-  assert,equal(l.length(),0)
+    var l = new List();
+    assert.equal(l.length(), 0);
+    l.add("a");
+    l.add("b");
+    l.add("c");
+    assert.equal(l.length(), 3);
+    l.pop();
+    assert.equal(l.length(), 2);
+    l.pop();
+    l.pop();
+    assert.equal(l.length(), 0);
 });
 
 QUnit.test("Pop gets the most recent element off the list", function (assert) {
-  var l = new List()
-  l.add("a")
-  l.add("b")
-  l.add("c")
+    var l = new List();
+    l.add("a");
+    l.add("b");
+    l.add("c");
 
-  assert.equal(l.pop(),"c")
-  assert.equal(l.head(),"b")
-  assert.equal(l.length(), 2)
+    assert.equal(l.pop(), "c");
+    assert.equal(l.head(), "b");
+    assert.equal(l.length(), 2);
 
-  assert.equal(l.pop(),"b")
-  assert.equal(l.head(),"a")
-  assert.equal(l.length(), 1)
+    assert.equal(l.pop(), "b");
+    assert.equal(l.head(), "a");
+    assert.equal(l.length(), 1);
 
-  assert.equal(l.pop(),"a")
-  assert.equal(l.head(),null)
-  assert.equal(l.length(), 0)
+    assert.equal(l.pop(), "a");
+    assert.equal(l.head(), null);
+    assert.equal(l.length(), 0);
 
-  // Any further pop()s return null 
-  assert.equal(l.pop(),null)
-  assert.equal(l.head(),null)
-  assert.equal(l.empty(), true)
+    // Any further pop()s return null
+    assert.equal(l.pop(), null);
+    assert.equal(l.head(), null);
+    assert.equal(l.empty(), true);
 });
 
 QUnit.test("Returns the last element in the list", function (assert) {
-  var l = new List()
-  assert.equal(l.last(), null)
-  l.add("a")
-  l.add("b")
-  l.add("c")
-  assert.equal(l.last(),"a")
-  assert.equal(l.length(), 3)
+    var l = new List();
+    assert.equal(l.last(), null);
+    l.add("a");
+    l.add("b");
+    l.add("c");
+    assert.equal(l.last(), "a");
+    assert.equal(l.length(), 3);
 });
 
-QUnit.test("Sorts a list alphabetically", function(assert) {
-  var l = new List()
-  l.sort() // Must not fail on empty list
-  assert.equal(l.last(), null)
-  l.add("c")
-  l.add("a")
-  l.add("b")
-  l.add("a")
-  l.sort()
-  assert.equal(l.pop(),"a")
-  assert.equal(l.pop(),"a")
-  assert.equal(l.pop(),"b")
-  assert.equal(l.pop(),"c")
+QUnit.test("Sorts a list alphabetically", function (assert) {
+    var l = new List();
+    l.sort(); // Must not fail on empty list
+    assert.equal(l.last(), null);
+    l.add("c");
+    l.add("a");
+    l.add("b");
+    l.add("a");
+    l.sort();
+    assert.equal(l.pop(), "a");
+    assert.equal(l.pop(), "a");
+    assert.equal(l.pop(), "b");
+    assert.equal(l.pop(), "c");
 
+});
+
+QUnit.test("Sorts a list of names alphabetically", function (assert) {
+    var l = new List();
+    l.add("Nick");
+    l.add("Dave");
+    l.add("Cara");
+    l.add("Andre");
+
+    l.sort();
+
+    assert.equal(l.pop(), "Andre");
+    assert.equal(l.pop(), "Cara");
+    assert.equal(l.pop(), "Dave");
+    assert.equal(l.pop(), "Nick");
+
+    assert.equal(l.length(), 0);
+});
+
+QUnit.test("Sorts a list of numbers alphabetically", function (assert) {
+    var l = new List();
+
+    l.add("4");
+    l.add("5");
+    l.add("9");
+    l.add("7");
+    l.add("1");
+    l.add("3");
+
+    l.sort();
+
+    assert.equal(l.pop(), "1");
+    assert.equal(l.pop(), "3");
+    assert.equal(l.pop(), "4");
+    assert.equal(l.pop(), "5");
+    assert.equal(l.pop(), "7");
+    assert.equal(l.pop(), "9");
 });
--- list.js
+++ list.js
@@ -1,58 +1,91 @@
-
 // An object to hold the item we will be
 // storing in our list
-var ListItem = function(data, tail) {
-  this.tail = tail
-  this.data = data
-}
+var ListItem = function (data, tail) {
+    this.tail = tail;
+    this.data = data;
+};
 
 // List implementation
-var List = function() {
-  this.item = null
+var List = function () {
+    this.item = null;
 
-  // Add a data item to the front of the list
-  this.add = function(elem) {
-    //this.item = new ListItem(elem, this.item)
-  }
+    // Add a data item to the front of the list
+    this.add = function (elem) {
+        this.item = new ListItem(elem, this.item);
+        this.count++;
+    };
 
-  // Returns true if the list is empty
-  this.empty = function() {
-    return this.item === null
-  }
+    // Returns true if the list is empty
+    this.empty = function () {
+        return this.item == null;
+    };
 
-  // Returns the data at the head of the list. If
-  // the list is empty, return null. This leaves
-  // the list unmodified
-  this.head = function() {
-    // ......
-    return null
-  }
+    // Returns the data at the head of the list. If
+    // the list is empty, return null. This leaves
+    // the list unmodified
+    this.head = function () {
+        return this.item == null ? null : this.item.data;
+    };
 
-  // Remove item off the head of the list and return
-  // its value. The new head of the list must be the
-  // next element in the list if it exists. If the
-  // list is empty, we return null
-  this.pop = function() {
-    // ......
-    return null
-  }
+    // Remove item off the head of the list and return
+    // its value. The new head of the list must be the
+    // next element in the list if it exists. If the
+    // list is empty, we return null
+    this.pop = function () {
+        if (this.empty())
+            return null;
 
-  // Return the number of elements in the list.
-  this.length = function() {
-    // ......
-    return -1
-  }
+        var head = this.item.data;
+        this.item = this.item.tail;
 
-  // Return the last data item in the list if it exists. If
-  // not, return null
-  this.last = function() {
-    // ......
-    return null
-  }
+        return head;
+    };
 
-  // In place sort of the list from smallest to largest
-  this.sort = function() {
-    //........
-  }
-}
+    // Return the number of elements in the list.
+    this.length = function () {
+        var count = 0;
+        for (var p = this.item; p != null; p = p.tail)
+            count++;
+
+        return count;
+    };
+
+    // Return the last data item in the list if it exists. If
+    // not, return null
+    this.last = function () {
+        var last = null;
+        for (var p = this.item; p != null; p = p.tail)
+            last = p;
+        return last == null ? null : last.data;
+    };
+
+    // In place sort of the list from smallest to largest
+    this.sort = function () {
+        var tmp = null;
+        var didSwap = true;
+        var pass = 1, numSwaps = 0;
+
+        // Handle empty lists & 1 element lists upfront.
+        if (this.item == null || (this.item != null && this.item.tail == null))
+            return;
+
+        console.log("List#sort: Sorting " + this.length() + " elements...");
+        while (didSwap) {
+            didSwap = false;
+
+            console.log("List#sort: pass = " + pass);
+            for (p = this.item; p.tail != null; p = p.tail) {
+                if (p.data.localeCompare(p.tail.data) > 0) {
+                    tmp = p.data;
+                    p.data = p.tail.data;
+                    p.tail.data = tmp;
+                    didSwap = true;
+                    ++numSwaps;
+                }
+            }
+            pass++;
+        }
+        console.log("List#sort: numSwaps = "+ numSwaps);
+    };
+};
 
